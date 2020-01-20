753b5dfbbb83f8e0d6af15a11dba2c2627030d5f
Jeff Yates <jeff@somewhatabstract.com>
2020-01-19 20:24:42 -0600

    Add react-router-dom and some routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-links">
+            <Link className="App-link" to="/">Home</Link>
+            <Link className="App-link" to="/about">About</Link>
+            <Link className="App-link" to="/contact">Contact</Link>
+          </div>
+        </header>
+        <section className="App-content">
+          <Switch>
+            <Route path="/about">
+              This is the about page!
+            </Route>
+            <Route path="/contact">
+              This is the contact page!
+            </Route>
+            <Route path="/">
+              This is the home page!
+            </Route>
+          </Switch>
+        </section>
+      </div>
+    </BrowserRouter>
   );
 }
 
