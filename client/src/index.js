83d91d5a75f27630bbf7bfb69538d7a03d9185d9
Jeff Yates <jeff@somewhatabstract.com>
2020-03-15 17:48:41 -0500

    Add hydration to client

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const mountElement = document.getElementById('root');
+const reactMountFn = (mountElement.childElementCount === 0)
+    ? ReactDOM.render
+    : ReactDOM.hydrate;
+
+reactMountFn(<App />, mountElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
